338aa376d84fd52a83d2702c13acb49ad46739fe
DJCool360 <fuzzyhead5555@gmail.com>
2026-03-04 18:00:36 -0500

    Add files via upload

---
+++ playerController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+  SerializeField] playerController controller;
+  [SerializeField] LayerMask ignoreLayer;
+
+  [SerializeField] int HP;
+  [SerializeField] int speed;
+  [SerializeField] int sprintMod;
+  [SerializeField] int jumpSpeed;
+  [SerializeField] int jumpMax;
+  [SerializeField] int gravity;
+
+  [SerializeField] int shootDamage;
+  [SerializeField] int shootDist;
+  [SerializeField] float shootRate;
+
+  int jumpCount;
+
+  int HPOrig;
+
+  float shootTimer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
