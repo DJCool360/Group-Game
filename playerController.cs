28914f5c160d9a88c312d7efee34b7890af3f8d2
DJCool360 <fuzzyhead5555@gmail.com>
2026-03-07 21:14:26 -0500

    Add files via upload

--- playerController.cs
+++ playerController.cs
@@ -48,6 +48,29 @@ public class playerController : MonoBehaviour
     HP = HPOrig;
     updatePlayerUI();
   }
-  
-  
+
+  void movement()
+  {
+    shootTimer += Time.deltaTime;
+    Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+
+    if (controller.isGrounded)
+    {
+      jumpCount = 0;
+      playerVel = Vector3.zero;
+    }
+
+    //moveDir = new Vector3(Input.GetAxis("Horizontal") 0, Input.GetAxis("Vertical"));
+    moveDir = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
+    controller.Move(moveDir * speed * Time.deltaTime);
+
+    jump();
+    controller.Move(playerVel * Time.deltaTime);
+    playerVel.y -= gravity * Time.deltaTime;
+    if (Input.GetButton("Fire1") && gunList.Count > 0 && gunList[gunListPos].ammoCur > 0 && shootTimer >= shootRate)
+      shoot();
+      selectGun();
+      reload();
+  }
+
 }
